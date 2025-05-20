8617a5be375b5e7e9d1eddf454a589518fcbcbe7
Deli Sandor <delisandor@Delis-MacBook-Pro.local>
2025-05-20 17:37:27 +0200

    feat/berlin-api-posts-redux-done

--- client/src/components/Posts/Timeline.tsx
+++ client/src/components/Posts/Timeline.tsx
@@ -3,21 +3,22 @@ import { useScrollFetchRest } from "../../hooks/useScrollFetchRest";
 import { Post } from "./Post";
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../store/userSlice';
+import { fetchPostsAction } from '../../store/postActions';
+import { selectPosts } from '../../store/postSlice';
 
 export const Timeline = () => {
   const user = useSelector(selectUser);
-  const { data, refAnchor, noMoreData } = useScrollFetchRest({ userId: user.id });
-
-  if (!data) return null;
+  const posts = useSelector(selectPosts);
+  const { refAnchor, noMoreData } = useScrollFetchRest({ userId: user.id });
 
   return (
     <Container maxWidth="sm">
-      {data.map((post) => (
+      {posts.map((post) => (
         <Post post={post} key={post.id} />
       ))}
       {noMoreData ? (
         <Typography textAlign="center" mt={2}>
-          No {data.length > 0 && "more "}posts
+          No {posts.length > 0 && "more "}posts
         </Typography>
       ) : (
         <Typography textAlign="center" ref={refAnchor}>
